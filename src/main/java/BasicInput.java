26b811424da5d08d6fe6f0773d60a27bc9c35bb4
ravenhospie <152207421+ravenhospie@users.noreply.github.com>
2024-04-22 19:57:32 -0400

    Initial commit

---
+++ src/main/java/BasicInput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BasicInput {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Enter integer:");
+        int userInt = scnr.nextInt();
+
+        // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
+
+        // TODO (2): Output the four values in reverse
+
+        // TODO (3): Cast the double to an integer, and output that integer
+
+    }
+}
